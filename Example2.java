4afc0087ceecbd7d102ff8fe8e8bb365353ae1da
prasad <prasadmane268@gmail.com>
2024-01-22 19:40:59 +0530

    Example2

---
+++ Example2.java
@@ -0,0 +1,10 @@
+package com.java.Demo;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to git world-Example2");
+	}
+
+}
